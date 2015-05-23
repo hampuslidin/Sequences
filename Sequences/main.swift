6ba1eb60915174bdf9fa4ee9f54dd035f2859d93
Hampus L. (OSX) <hampuslidin@gmail.com>
2015-05-23 15:37:12 +0200

    Initial Commit

---
+++ Sequences/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sequences
+//
+//  Created by Hampus Lidin on 2015-05-23.
+//  Copyright (c) 2015 Lidin. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
