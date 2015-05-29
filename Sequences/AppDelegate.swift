5c858a20961fee333aa57989f45084ad0e9449b9
Hampus L. (OSX) <hampuslidin@gmail.com>
2015-05-29 10:17:27 +0200

    Added application files.

---
+++ Sequences/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Sequences
+//
+//  Created by Hampus Lidin on 2015-05-27.
+//  Copyright (c) 2015 Lidin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
